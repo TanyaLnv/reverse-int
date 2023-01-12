822fd080fdaa1d6931e1d1d0d9405459ba96ae54
TanyaLnv <115620110+TanyaLnv@users.noreply.github.com>
2023-01-12 18:24:23 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
+  n = Math.abs(n);
+  let str = "" + n;
+  let num = "";
   
+   for (let i = str.length - 1; i >= 0; i--) { 
+    num = `${num}${str[i]}`;
+    }
+ 
+  return Number(num);
 }
